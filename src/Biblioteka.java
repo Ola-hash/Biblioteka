171d2fac054ec5b36741d76ca2ab99002e3004df
Ola <ola.nauka@gmail.com>
2020-10-23 20:31:43 +0200

    Dodanie metody wczytajZPliku w klasie Biblioteka.

--- src/Biblioteka.java
+++ src/Biblioteka.java
@@ -1,30 +1,34 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Biblioteka {
     ArrayList<Ksiazka> ksiazki;
+    String path;
 
     public Biblioteka() {
         ksiazki = new ArrayList<>();
     }
 
     public void wypozycz(Ksiazka ksiazka) {
-            if (ksiazki.contains(ksiazka)) {
-                if (ksiazka.czyWypozyczona()) {
-                    System.out.println("Nie mozna wypozyczyć, książka została już wypożyczona.");
-                } else {
-                    ksiazka.setCzyWypozyczona(true);
-                    System.out.println("Ksiązka została wypożyczona.");
-                }
+        if (ksiazki.contains(ksiazka)) {
+            if (ksiazka.czyWypozyczona()) {
+                System.out.println("Nie mozna wypozyczyć, książka została już wypożyczona.");
             } else {
-                System.out.println("Książki nie ma w bibliotece.");
+                ksiazka.setCzyWypozyczona(true);
+                System.out.println("Ksiązka została wypożyczona.");
             }
+        } else {
+            System.out.println("Książki nie ma w bibliotece.");
         }
+    }
 
     public void zwrocKsiazke(Ksiazka ksiazka) {
         if (ksiazki.contains(ksiazka)) {
             ksiazka.setCzyWypozyczona(false);
             System.out.println("Ksiązka została zwrocona.");
-        }else {
+        } else {
             System.out.println("Podanej książki nie ma w bibiotece.");
         }
     }
@@ -37,6 +41,29 @@ public class Biblioteka {
 
     public void dodajKsiazke(Ksiazka ksiazka) {
         ksiazki.add(ksiazka);
+    }
+
+    public void wczytakKsiazkiZPliku(String path) {
+        try {
+            File file = new File(path);
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNextLine()) {
+                String data = scanner.nextLine();
+                String array[] = data.split(";");
+                for (int i = 0; i < array.length; i++) {
+                    String[] books = array[i].split("-");
+                    String autor = books[0];
+                    String tytul = books[1];
+                    Ksiazka ksiazka=new Ksiazka(tytul,autor);
+                    dodajKsiazke(ksiazka);
+                }
+            }
+
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("Nie ma takiego pliku.");
+        }
+
     }
 
 
